75c1e9a7a9d9254b69fd82b0e7686af0d63c2c16
Fortune Ekeruo <legalmody@gmail.com>
2017-10-25 16:23:56 +0100

    Register and use footer component and rename footer template properly

--- app/components/index.js
+++ app/components/index.js
@@ -6,8 +6,10 @@ import componentRoutes from './index.routes';
 
 // Components
 import { navbarComponent } from './shared/navbar/navbar.component';
+import { footerComponent } from './shared/footer/footer.component';
 
 export default angular.module('rootComponent', [uiRouter])
     .config(componentRoutes)
     .component('navbarComponent', navbarComponent)
+    .component('footerComponent', footerComponent)
     .name;
