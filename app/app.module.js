05f2292b62307d4fc5367f4a15f63895db53c79a
Fortune Ekeruo <legalmody@gmail.com>
2017-10-25 14:33:32 +0100

    Remove imported deleted component:

--- app/app.module.js
+++ app/app.module.js
@@ -2,8 +2,6 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 
 import routes from './app.routes';
-import baseComponents from './Components'
-// import home from './Components/HomePage/home.module';
 
-angular.module('App', [uirouter, baseComponents])
+angular.module('App', [uirouter])
 .config(routes);
