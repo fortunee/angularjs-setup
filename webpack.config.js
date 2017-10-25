bda15d74f1a9f52304919edc9e392e240138ffb0
Fortune Ekeruo <legalmody@gmail.com>
2017-10-25 16:34:41 +0100

    Refactor webpack config to bundle sass files and install the extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './app/app.module.js',
@@ -8,21 +9,36 @@ module.exports = {
         filename: 'app.bundle.js',
         publicPath: '/assets'
     },
-    devServer: {
-        inline: true,
-        contentBase: __dirname + '/bin/assets',
-        port: 9090
-    },
+
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loaders: 'babel-loader',
             },
-            { test: /\.html$/, loader: 'html' },
-            { test: /\.css$/, loader: 'style!css' }
+
+            { 
+                test: /\.html$/, loader: 'html'
+            },
+
+            {
+                test: /\.scss$/,
+                // loaders: ['style-loader', 'css-loader', 'sass-loader']
+                loader: ExtractTextPlugin.extract({
+                    fallbackLoader: 'style-loader',
+                    loader: 'css-loader!sass-loader',
+                })
+            }
         ]
     },
+
+    plugins: [
+        new ExtractTextPlugin('styles.bundle.css'),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ],
+
     devtool: '#inline-source-map'
 }
