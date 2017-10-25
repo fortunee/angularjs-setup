85e58b63bc741f4f4163e999ebc1e5b014828eca
Fortune Ekeruo <legalmody@gmail.com>
2017-10-25 15:00:34 +0100

    Import routes and register it in the config

--- app/components/index.js
+++ app/components/index.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
+// Routes
+import routes from './index.routes';
+
 export default angular.module('rootComponent', [uiRouter])
-    .config()
+    .config(routes)
     .name;
-    
