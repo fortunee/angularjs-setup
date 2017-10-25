6b847378859aa61a5a0ef889843e392260fd2897
Fortune Ekeruo <legalmody@gmail.com>
2017-10-25 15:47:59 +0100

    Register and use navbar component

--- app/components/index.js
+++ app/components/index.js
@@ -4,6 +4,10 @@ import uiRouter from 'angular-ui-router';
 // Component Routes
 import componentRoutes from './index.routes';
 
+// Components
+import { navbarComponent } from './shared/navbar/navbar.component';
+
 export default angular.module('rootComponent', [uiRouter])
     .config(componentRoutes)
+    .component('navbarComponent', navbarComponent)
     .name;
