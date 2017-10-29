33c9c6cd4e05ffd47954c8cda116132661266ea0
Fortune Ekeruo <legalmody@gmail.com>
2017-10-29 19:52:17 +0100

    Add specs.webpack.js file to karma and webpack config for bundling and running the test

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      './specs/specs.webpack.js'
     ],
 
 
@@ -30,6 +31,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      './specs/specs.webpack.js': ['webpack', 'sourcemap'],
     },
 
 
@@ -64,6 +66,9 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
+
+    // Webpack config
+    webpack: webpackConfig,
 
     // Concurrency level
     // how many browser should be started simultaneous
