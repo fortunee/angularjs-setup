9e13f37d292fdaf5b58875382922ce7b12896d1c
Fortune Ekeruo <legalmody@gmail.com>
2017-10-29 19:04:55 +0100

    Register the about component

--- app/components/index.js
+++ app/components/index.js
@@ -8,8 +8,11 @@ import componentRoutes from './index.routes';
 import { navbarComponent } from './shared/navbar/navbar.component';
 import { footerComponent } from './shared/footer/footer.component';
 
+import { aboutComponent } from './about/about.component';
+
 export default angular.module('rootComponent', [uiRouter])
     .config(componentRoutes)
     .component('navbarComponent', navbarComponent)
+    .component('aboutComponent', aboutComponent)
     .component('footerComponent', footerComponent)
     .name;
