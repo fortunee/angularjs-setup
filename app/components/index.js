7e0a59d7725e215d51d21a048c172bf06ed548c1
Fortune Ekeruo <legalmody@gmail.com>
2017-10-31 13:05:26 +0100

    Import and register app services

--- app/components/index.js
+++ app/components/index.js
@@ -4,6 +4,9 @@ import uiRouter from 'angular-ui-router';
 // Component Routes
 import componentRoutes from './index.routes';
 
+// Services
+import AppService from './../services/app.service';
+
 // Components
 import { navbarComponent } from './shared/navbar/navbar.component';
 import { footerComponent } from './shared/footer/footer.component';
@@ -12,6 +15,7 @@ import { aboutComponent } from './about/about.component';
 
 export default angular.module('rootComponent', [uiRouter])
     .config(componentRoutes)
+    .service('AppService', AppService)
     .component('navbarComponent', navbarComponent)
     .component('aboutComponent', aboutComponent)
     .component('footerComponent', footerComponent)
